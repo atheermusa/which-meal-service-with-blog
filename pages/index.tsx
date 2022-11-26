e37e782d6c6125c997d6f8518b7ece24cbf340ba
atheermusa <44138770+atheermusa@users.noreply.github.com>
2022-11-26 18:27:53 +0000

    update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,38 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../interfaces/post'
+import React from "react";
 
-type Props = {
-  allPosts: Post[]
-}
+import AboutUs from "../components/about-us";
+import Canvas from "../components/canvas";
+import Header from "../components/header-original";
+import HowItWorks from "../components/how-it-works";
+import LazyShow from "../components/lazy-show";
+import MainHero from "../components/main-hero";
+import MainHeroImage from "../components/main-hero-image";
 
-export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
-}
+const App = () => {
+    return (
+        <div className={`bg-background grid gap-y-8 overflow-hidden`}>
+            <div className={`relative bg-background`}>
+                <div className="max-w-7xl mx-auto">
+                    <div
+                        className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
+                    >
+                        <Header />
+                        <MainHero />
+                    </div>
+                </div>
+                <MainHeroImage />
+            </div>
+            <LazyShow>
+                <HowItWorks />
+            </LazyShow>
+            <LazyShow>
+                <>
+                    <AboutUs />
+                    <Canvas />
+                </>
+            </LazyShow>
+        </div>
+    );
+};
 
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
-
-  return {
-    props: { allPosts },
-  }
-}
+export default App;
