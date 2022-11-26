2c2907bea6c5d659369f6604ad40cf49c1e83344
atheermusa <44138770+atheermusa@users.noreply.github.com>
2022-11-26 19:14:51 +0000

    update card group styling

--- components/card-group.tsx
+++ components/card-group.tsx
@@ -6,7 +6,7 @@ import Card from "./card";
 const CardGroup = () => {
     const { cardsContent } = config;
     return (
-        <div className="flex flex-col	md:flex-row md:space-x-4">
+        <div className="flex flex-col space-y-4	lg:flex-row lg:space-x-4">
             {cardsContent.map((content) => (
                 <Card content={content} key={content.name} />
             ))}
