ab2bc714c29895eb9d3c9b9f00600d1b0a538159
atheermusa <44138770+atheermusa@users.noreply.github.com>
2022-11-27 21:15:51 +0000

    removing unused code on landing page hero

--- components/main-hero.tsx
+++ components/main-hero.tsx
@@ -22,20 +22,12 @@ const MainHero = () => {
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                     <div className="rounded-md shadow">
                         <a
-                            href="https://pcmttx0aixz.typeform.com/to/DcqfdFGO"
+                            href="https://l4p5yx6nazw.typeform.com/to/M2QcZXtW"
                             className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border md:py-4 md:text-lg md:px-10`}
                         >
                             {mainHero.primaryAction.text}
                         </a>
                     </div>
-                    {/* <div className="mt-3 sm:mt-0 sm:ml-3">
-            <a
-              href={mainHero.secondaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:text-primary md:py-4 md:text-lg md:px-10`}
-            >
-              {mainHero.secondaryAction.text}
-            </a>
-          </div> */}
                 </div>
             </div>
         </main>
